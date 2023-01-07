ddb3ec0b63fd1048e90b6534a503b482382c27ec
ErdiDemir <erdi.demir@icloud.com>
2023-01-07 12:05:01 +0300

    Microsoft.AspNetCore.Identity user ve role eklendi. jwt ayarları appsetting.json eklendi.

---
+++ TravelMate.Domain/TravelMate.Domain/Entities/Authentications/Role.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TravelMate.Domain.Entities.Authentications
+{
+    /// <summary>
+    ///  This class inherents from Microsoft.AspNetCore.Identity.IdentityRole
+    ///  int type used for the primary key for the role.
+    ///  Represents a role in the identity system
+    /// </summary>
+    public class Role : IdentityRole<int>
+    {
+    }
+}
---
+++ TravelMate.Domain/TravelMate.Domain/Entities/Authentications/User.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TravelMate.Domain.Entities.Authentications
+{
+    /// <summary>
+    ///  This class inherents from Microsoft.AspNetCore.Identity.IdentityUser
+    ///  int type used for the primary key for the role.
+    ///  Represents an user in the identity system
+    /// </summary>
+    public class User : IdentityUser<int>
+    {
+       
+        public string FullName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets user first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Gets or sets user last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string VerifyCode { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime VerifyDate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool Verified { get; set; }
+
+        /// <summary>
+        /// Gets or sets Last Login Date 
+        /// if there is no login for x days, user have to delete
+        /// </summary>
+        public DateTime LastLoginDate { get; set; }
+
+      }
+}
